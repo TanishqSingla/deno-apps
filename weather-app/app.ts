d7e5e8bf94e890da640892067aa83ef51921943e
TanishqSingla <tanishqsingla00@gmail.com>
2021-03-28 13:32:25 +0530

    using interface for weather data
    using async await sugar syntax for api request

--- weather-app/app.ts
+++ weather-app/app.ts
@@ -1,27 +1,31 @@
 import { key } from "./secrets.ts";
 
+interface weatherData {
+  main: {
+    temp: number;
+  };
+  weather: {
+    0: {
+      id: string;
+      description: string;
+    };
+  };
+}
+
 const location: string = Deno.args[0];
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
 
-if (location) {
-  const data = fetch(url, { method: "GET" });
+const weatherReq = async (): Promise<weatherData> => {
+  const response = await fetch(url);
+  return response.json();
+};
 
-  data
-    .then((res) => {
-      return res.json();
-    })
-    .then((weatherData) => {
-      const description = weatherData?.weather[0]?.description;
-      const temp: number = weatherData?.main?.temp;
+const data: weatherData = await weatherReq();
 
-      const tempInCels = (temp - 273).toFixed(2);
+const temperature = data.main.temp;
+const description = data.weather[0].description;
 
-      console.log(`The temperature in ${location} is ${tempInCels}C`);
-      console.log(`It is ${description} today in ${location}`);
-    })
-    .catch((e) => {
-      console.log("ERROR: request not handled");
-    });
-} else {
-  console.log("LOCATION NOT SPECIFIED");
-}
+const tempInCels = (temperature - 273).toFixed(2);
+
+console.log(`The temperature in ${location} is ${tempInCels}C`);
+console.log(`It is ${description} today in ${location}`);
